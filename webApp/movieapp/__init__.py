e55176c3bd3ae92b4e8e52a3b72fe6438198da2a
Alberto <ambr0s3@users.noreply.github.com>
2017-04-18 00:29:26 -0400

    App Completada

---
+++ webApp/movieapp/__init__.py
@@ -0,0 +1,41 @@
+from flask import abort, Flask, g, render_template, request, send_from_directory
+from flask_cors import CORS
+from movieapp.middlewares.prefix import PrefixMiddleware
+from movieapp.main.routes import main
+from movieapp.config import configure_application
+from movieapp.data.models import db
+
+application = Flask(__name__, static_folder="static")
+application.wsgi_app = PrefixMiddleware(application.wsgi_app, prefix='/alberto')
+
+CORS(application)
+configure_application(application)
+db.init_app(application)
+
+with application.app_context():
+    db.create_all()
+
+@application.route("/")
+def home():
+  return application.send_static_file("reviews.html");
+
+@application.route('/<path:filename>')
+def serve_html(filename):
+    return send_from_directory("static", filename, as_attachment=False)
+
+@application.route('/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory("static/css", filename, as_attachment=False)
+
+@application.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory("static/js", filename, as_attachment=False)
+
+@application.errorhandler(500)
+def page_not_found(error):
+    return "error interno manito", 500
+
+application.register_blueprint(main)
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
---
+++ webApp/movieapp/config.py
@@ -0,0 +1,36 @@
+from movieapp.data.models import db
+import os
+
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    # sqlite :memory: identifier is the default if no filepath is present
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///movie_db.db'
+    SECRET_KEY = '1d94e52c-1c89-4515-b87a-f48cf3cb7f0b'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///movie_db.db'
+    SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39f'
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SECRET_KEY = '792842bc-c4df-4de1-9177-d5207bd9faa6'
+
+config = {
+    "development": "movieapp.config.DevelopmentConfig",
+    "testing": "movieapp.config.TestingConfig",
+    "default": "movieapp.config.DevelopmentConfig"
+}
+
+
+def configure_application(application):
+    config_name = os.getenv('FLASK_CONFIGURATION', 'default')
+    application.config.from_object(config[config_name])
+
